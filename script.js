7011596edaf0a7a32a40bf7beb8ab5eb218384c2
js-bots <kacpernegr1@gmail.com>
2020-09-30 12:41:09 +0200

    RobotaZrobiona

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const btn = document.querySelector(".main-text-button");
-const popup = document.querySelector('.pop-up-bg');
+const popupbg = document.querySelector('.pop-up-bg');
+const popup = document.querySelector('.pop-up');
+const mobileBtn = document.querySelector('.header-mobile-button');
+const mobileNav = document.querySelector('.header-nav-wrapper');
 
-btn.addEventListener('click', (e) => {
-    popup.classList.add('show')
-})
+btn.addEventListener('click', e => {
+    popupbg.classList.add('show');
+});
 
-popup.addEventListener('click', () => {
-    popup.classList.remove('show')
+popupbg.addEventListener('click', e => {
+    popupbg.classList.remove('show'); 
+});
+
+popup.addEventListener('click', e => {
+    e.stopPropagation();
+});
+
+mobileBtn.addEventListener('click', e => {
+    mobileBtn.classList.toggle('active');
+    mobileNav.classList.toggle('active');
 })
